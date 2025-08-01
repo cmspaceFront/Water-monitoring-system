0ea6cfd945ce43c49ce37d379f8fb4a97d19ad40
FirdavsRustamovSh <firdavsrustamovsh@gmail.com>
2025-08-01 16:51:36 +0500

    first commit

---
+++ grid-info/dist/runtime/translations/translations.ts
@@ -0,0 +1,421 @@
+// translations.ts
+
+export const attributeTranslations = {
+  uz: {
+    fid: "FID",
+    id: "ID",
+    area: "Maydon",
+    orig_fid: "Orig FID",
+    balandlik: "Balandlik",
+    hajm: "Hajm",
+    sana: "Sana",
+    suv_ombori: "Suv ombori",
+    yil: "Yil",
+    globalid: "Global ID",
+    xisobot: "Hisobot",
+    shape_leng: "Perimetr",
+    shape_le_1: "Perimetr 2",
+    globalid_1: "Global ID 2",
+    SHAPE__Length: "Poligon uzunligi",
+    SHAPE__Area: "Poligon maydoni",
+    sensor: "Sensor",
+    son: "Son",
+  },
+  ru: {
+    fid: "FID",
+    id: "ID",
+    area: "Площадь",
+    orig_fid: "Оригинальный FID",
+    balandlik: "Высота",
+    hajm: "Объем",
+    sana: "Дата",
+    suv_ombori: "Водохранилище",
+    yil: "Год",
+    globalid: "Глобальный ID",
+    xisobot: "Отчет",
+    shape_leng: "Периметр",
+    shape_le_1: "Периметр 2",
+    globalid_1: "Глобальный ID 2",
+    SHAPE__Length: "Длина полигона",
+    SHAPE__Area: "Площадь полигона",
+    sensor: "Сенсор",
+    son: "Количество",
+  },
+  uzcryl: {
+    fid: "FID",
+    id: "ID",
+    area: "Майдон",
+    orig_fid: "Оригинал FID",
+    balandlik: "Баландлик",
+    hajm: "Ҳажм",
+    sana: "Сана",
+    suv_ombori: "Сув омбори",
+    yil: "Йил",
+    globalid: "Глобал ID",
+    xisobot: "Ҳисобот",
+    shape_leng: "Периметр",
+    shape_le_1: "Периметр 2",
+    globalid_1: "Глобал ID 2",
+    SHAPE__Length: "Полигон узунлиги",
+    SHAPE__Area: "Полигон майдони",
+    sensor: "Сенсор",
+    son: "Сон",
+  }
+};
+
+// Локализации названий дамб
+export const dambNameTranslations = {
+    todakol: {
+      uz: "To'dako'l suv ombori",
+      ru: "Водохранилище Тудакуль",
+      uzcryl: "Тўдакўл сув омбори",
+    },
+    chortoq:{
+      uz: "Chortoq suv ombori",
+      ru: "Водохранилище Чорток",
+      uzcryl: "Чортоқ сув омбори",
+    },
+    topalang:{
+      uz: "To'palang suv ombori",
+      ru: "Водохранилище Топаланг",
+      uzcryl: "Тўпаланг сув омбори",
+    },
+    tusunsoy:{ //tusunsoy
+      uz: "To'sinsoy suv ombori",
+      ru: "Водохранилище Тосинсой",
+      uzcryl: "Тўсиной сув омбори",
+    },
+    tosinsoy:{
+      uz: "To'sinsoy suv ombori",
+      ru: "Водохранилище Тосинсой",
+      uzcryl: "Тўсиной сув омбори",
+    },
+    toshkent:{
+      uz: "Toshkent suv ombori",
+      ru: "Водохранилище Ташкент",
+      uzcryl: "Тошкент сув омбори",
+    },
+    talimarjon:{
+      uz: "Talimarjon suv ombori",
+      ru: "Водохранилище Талимарджон",
+      uzcryl: "Талимаржон сув омбори",
+    },
+    kattakorgon:{ //kattaqorgon
+      uz: "Kattaqo'rg'on suv ombori",
+      ru: "Водохранилище Каттакоргон",
+      uzcryl: "Каттақўрғон сув омбори",
+    },
+    zarkent:{
+      uz: "Zarkent suv ombori",
+      ru: "Водохранилище Заркент",
+      uzcryl: "Заркент сув омбори",
+    }, //yo'q
+    zomin:{
+      uz: "Zomin suv ombori",
+      ru: "Водохранилище Зомин",
+      uzcryl: "Зомин сув омбори",
+    },
+    qorovultepa:{
+      uz: "Qorovultepa suv ombori",
+      ru: "Водохранилище Коровултепа",
+      uzcryl: "Қоровултепа сув омбори",
+    },
+    oktepa:{ //oqtepa
+      uz: "Oqtepa suv ombori",
+      ru: "Водохранилище Октепа",
+      uzcryl: "Оқтепа сув омбори",
+    },
+    jiydalisoy:{
+      uz: "Jiydalisoy suv ombori",
+      ru: "Водохранилище Джийдалисой",
+      uzcryl: "Джийдалисой сув омбори",
+    },
+    ohangaron:{
+      uz: "Ohangaron suv ombori",
+      ru: "Водохранилище Охангарон",
+      uzcryl: "Оҳангарон сув омбори",
+    },
+    pachkamar:{
+      uz: "Pachkamar suv ombori",
+      ru: "Водохранилище Пачкамар",
+      uzcryl: "Пачкамар сув омбори",
+    },
+    karkidon:{
+      uz: "Karkidon suv ombori",
+      ru: "Водохранилище Каркиндон",
+      uzcryl: "Каркиндон сув омбори",
+    },
+    jizzax:{ //jizzax 
+      uz: "Jizzax suv ombori",
+      ru: "Водохранилище Джизах",
+      uzcryl: "Джизах сув омбори",
+    },
+    hisorak:{
+      uz: "Hisorak suv ombori",
+      ru: "Водохранилище Хисорак",
+      uzcryl: "Ҳисорак сув омбори",
+    },
+    qorgontepa:{//qurgontepa
+      uz: "Qo'rg'ontepa suv ombori",
+      ru: "Водохранилище Коргонтепа",
+      uzcryl: "Қўрғонтепа сув омбори",
+    },
+    chorvoq:{
+      uz: "Chorvoq suv ombori",
+      ru: "Водохранилище Чорвок",
+      uzcryl: "Чорвоқ сув омбори",
+    },
+    quyumozor:{//quyimozor
+      uz: "Quyimozor suv ombori",
+      ru: "Водохранилище Куйимазар",
+      uzcryl: "Қўйимозор сув омбори",
+    },
+    oqdaryo:{
+      uz: "Oqdaryo suv ombori",
+      ru: "Акдарьинское водохранилище",
+      uzcryl: "Оқдарё сув омбори",
+    },
+    shurkul:{ //shorkol
+      uz: "Sho'rko'l suv ombori",
+      ru: "Шуркульское водохранилище",
+      uzcryl: "Шўркўл сув омбори",
+    },
+    qorasuv:{
+      uz: "Qorasuv suv ombori(Namangan)",
+      ru: "Корасувсукое водохранилище(Наманган)",
+      uzcryl: "Қорасув сув омбори(Наманган)",
+    },
+    uchqizil:{
+      uz: "Uchqizil suv ombori",
+      ru: "Учкызылское водохранилище",
+      uzcryl: "Учқизил сув омбори",
+    },
+    qorabog:{
+      uz: "Qorabog' suv ombori",
+      ru: "Карабагское водохранилище",
+      uzcryl: "Қорабоғ сув омбори",
+    },
+    nugayli:{
+      uz: "No'g'ayli suv ombori",
+      ru: "Нугайлийское водохранилище",
+      uzcryl: "Нўғайли сув омбори",
+    },
+    yangikurgan:{//yangiqorgon
+      uz: "Yangi Qo'rg'on suv ombori",
+      ru: "Янгикурганское водохранилище",
+      uzcryl: "Янги Қўрғон сув омбори",
+    },
+    qamashi:{ //kamashi
+      uz: "Qamashi suv ombori",
+      ru: "Камашинское водохранилище",
+      uzcryl: "Қамаши сув омбори",
+    },
+    sobirsoy:{
+      uz: "Sobirsoy suv ombori",
+      ru: "Сабирсайское водохранилище",
+      uzcryl: "Собирсой сув омбори",
+    },
+    degrez:{
+      uz: "Degrez suv ombori",
+      ru: "Дегрезское водохранилище",
+      uzcryl: "Дегрез сув омбори",
+    },
+    eskiyer:{
+      uz: "Eskiyer suv ombori",
+      ru: "Эскиерское водохранилище",
+      uzcryl: "Эскиер сув омбори",
+    },
+    girvonsoy:{ //girvonsoy
+      uz: "Girvansoy suv ombori",
+      ru: "Гирвансайское водохранилище",
+      uzcryl: "Гирвансой сув омбори",
+    },
+    oqbilol:{
+      uz: "Oqbilol suv ombori",
+      ru: "Окбилолское водохранилище",
+      uzcryl: "Оқбилол сув омбори",
+    },
+    kengkol:{
+      uz: "Kengko'l suv ombori",
+      ru: "Кенгкольское водохранилище",
+      uzcryl: "Кенгкўл сув омбори",
+    },
+    omondara:{ //omondora
+      uz: "Omondora suv ombori",
+      ru: "Водохранилище Омондора",
+      uzcryl: "Омондора сув омбори",
+    },
+    rezaksoy:{
+      uz: "Rezaksoy suv ombori",
+      ru: "Резаксойское водохранилище",
+      uzcryl: "Резаксой сув омбори",
+    },
+    qoratepa:{
+      uz: "Qoratepa suv ombori",
+      ru: "Водохранилище Каратепа",
+      uzcryl: "Қоратепа сув омбори",
+    },
+    kamongaron:{
+      uz: "Kamongaron suv ombori",
+      ru: "Водохранилище Камонгарон",
+      uzcryl: "Камонгарон сув омбори",
+    },
+    mominobod:{
+      uz: "Mominobod suv ombori",
+      ru: "Водохранилище Моминабад",
+      uzcryl: "Мўминобод сув омбори",
+    }, //yo'q
+    kokseraskoy:{
+      uz: "Ko'kseraksoy suv ombori",
+      ru: "Водохранилище Коксарексай",
+      uzcryl: "Кўксераксой сув омбори",
+    },
+    ertikon:{
+      uz: "Yertikon suv ombori",
+      ru: "Водохранилище Ертикан",
+      uzcryl: "Ертикон сув омбори",
+    },
+    langar:{
+      uz: "Langar suv ombori",
+      ru: "Лангарское водохранилище",
+      uzcryl: "Лангар сув омбори",
+    },
+    fargona:{
+      uz: "Markaziy Farg'ona suv ombori",
+      ru: "Центральное Ферганское водохранилище",
+      uzcryl: "Марказий Фарғона сув омбори",
+    },
+    qarasuv:{
+      uz: "Qorasuv suv ombori",
+      ru: "Водохранилище Карасув",
+      uzcryl: "Қорасув сув омбори",
+    },
+    qalqama:{
+      uz: "Qalqama sel ombori",
+      ru: "Водохранилище Калкама",
+      uzcryl: "Қалқама сел омбори",
+    },
+    shorabsoy:{
+      uz: "Sho'robsoy suv ombori",
+      ru: "Шурабсойское водохранилище",
+      uzcryl: "Шўробсой сув омбори",
+    },
+    dehqonobod:{
+      uz: "Dexqonobod suv ombori",
+      ru: "Дехканабадское водохранилище",
+      uzcryl: "Дехқонобод сув омбори",
+    },
+    sentabsoy:{ //sentobsoy
+      uz: "Sentobsoy suv ombori",
+      ru: "Водохранилище Сентобсой",
+      uzcryl: "Сентобсой сув омбори",
+    },
+    koksaroy:{
+      uz: "Ko'ksaroy suv ombori",
+      ru: "Коксаройское водохранилище",
+      uzcryl: "Кўксарой сув омбори",
+    },
+    xojamushkent:{
+      uz: "Xo'jamushkent suv ombori",
+      ru: "Ходжамушкентское водохранилище",
+      uzcryl: "Хўжамушкент сув омбори",
+    },
+    oqchobsoy:{
+      uz: "Oqchobsoy suv ombori",
+      ru: "Акчобсойское водохранилище",
+      uzcryl: "Oқчобсой сув омбори",
+    },
+    shorsuv:{
+      uz: "Sho'rsuv sel ombori",
+      ru: "Водохранилище Шорсув",
+      uzcryl: "Шўрсув сел омбори",
+    },
+    oqbuloq:{
+      uz: "Oqbuloq suv ombori",
+      ru: "Окбулокское водохранилище",
+      uzcryl: "Оқбулоқ сув омбори",
+    },
+    qizilsuv:{
+      uz: "Qizilsuv suv ombori",
+      ru: "Кызылсувское водохранилище",
+      uzcryl: "Қизилсув сув омбори",
+    },
+    asaka:{
+      uz: "Asaka adir suv ombori",
+      ru: "Водохранилище Асака адир",
+      uzcryl: "Асака адир сув омбори",
+    },
+    qoramurut:{
+      uz: "Qoramurut suv ombori",
+      ru: "Карамурутское водохранилище",
+      uzcryl: "Қорамурут сув омбори",
+    },
+    chimqurgon:{ //chimqorgon
+      uz: "Chimqo'rg'on suv ombori",
+      ru: "Чимкурганское водохранилище",
+      uzcryl: "Чимқўрғон сув омбори",
+    },
+    surxon:{ //surhon
+      uz: "Janubiy Surxon suv ombori",
+      ru: "Южно-Сурханское водохранилище",
+      uzcryl: "Жанубий Сурхон сув омбори",
+    },
+    arnasoy:{
+      uz: "Arnasoy suv ombori",
+      ru: "Арнасойское водохранилище",
+      uzcryl: "Арнасой сув омбори",
+    },
+    qashqadaryo:{
+      uz: "Qashqadaryo suv ombori",
+      ru: "Кашкадарьинское водохранилище",
+      uzcryl: "Қашқадарё сув омбори",
+    },
+    varziq:{ //varzik
+      uz: "Varziq suv ombori",
+      ru: "Водохранилище Варзик",
+      uzcryl: "Варзиқ сув омбори",
+    },
+    shorobsoy:{
+      uz: "Sho'robsoy suv ombori",
+      ru: "Шоробсойское водохранилище",
+      uzcryl: "Шўробсой сув омбори",
+    }
+  };
+
+// --- ALIAS: альтернативные написания ---
+export const ALIAS_MAP = {
+  tosinsoy: "tusunsoy",
+  kattakorgon: "kattaqorgon",
+  oktepa: "oqtepa",
+  jizzakh: "jizzax",
+  qorgontepa: "qurgontepa",
+  quyumozor: "quyimozor",
+  shurkul: "shorkol",
+  yangikurgan: "yangiqorgon",
+  qamashi: "kamashi",
+  girvansoy: "girvonsoy",
+  omondara: "omondora",
+  sentabsoy: "sentobsoy",
+  chimqurgon: "chimqorgon",
+  surxon: "surhon",
+  varziq: "varzik"
+};
+
+// Function to convert short dam name to full name
+export function getFullDamName(shortName: string, locale: string = 'uz'): string {
+  if (!shortName) return '';
+  
+  const normalizedName = shortName.toLowerCase().trim();
+  
+  // Check aliases first
+  const aliasedName = ALIAS_MAP[normalizedName] || normalizedName;
+  
+  // Get the translation
+  const damTranslation = dambNameTranslations[aliasedName];
+  if (damTranslation) {
+    return damTranslation[locale] || damTranslation.uz || shortName;
+  }
+  
+  // If no translation found, return original
+  return shortName;
+} 
